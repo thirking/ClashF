ed5542ddb56c519f0537ffc589f1c2117d1e4c92
thirking <xinhua_huang@roboticplus.com>
2022-12-27 00:18:19 +0800

    主页UI更新

--- ClashF/App.xaml.cs
+++ ClashF/App.xaml.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using ClashF.Services;
+using ClashF.View;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
+using Wpf.Ui.Mvvm.Contracts;
+using Wpf.Ui.Mvvm.Services;
 
 namespace ClashF
 {
@@ -13,12 +22,55 @@ namespace ClashF
     /// </summary>
     public partial class App : Application
     {
+        private static readonly IHost _host = Host
+            .CreateDefaultBuilder()
+            .ConfigureAppConfiguration(c => { c.SetBasePath(Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location)); })
+            .ConfigureServices((context, services) =>
+            {
+                // App Host
+                services.AddHostedService<ApplicationHostService>();
+
+                // Theme manipulation
+                services.AddSingleton<IThemeService, ThemeService>();
+
+                services.AddSingleton<IPageService, PageService>();
+
+                services.AddSingleton<INavigationService, NavigationService>();
+
+                services.AddSingleton<INotifyIconService, CustomNotifyIconService>();
+
+                services.AddScoped<INavigationWindow, MainView>();
+            }).Build();
+
         public App() => this.Activated += StartElmish;
+
+        private async void OnStartup(object sender, StartupEventArgs e)
+        {
+            await _host.StartAsync();
+        }
+
+        private async void OnExit(object sender, ExitEventArgs e)
+        {
+            await _host.StopAsync();
+
+            _host.Dispose();
+        }
+
+        public static T GetService<T>()
+        where T : class
+        {
+            return _host.Services.GetService(typeof(T)) as T;
+        }
 
         private void StartElmish(object? sender, EventArgs e)
         {
             this.Activated -= StartElmish;
             Program.main(MainWindow);
+        }
+
+        private void OnDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+
         }
     }
 }
--- ClashF/View/MainView.xaml.cs
+++ ClashF/View/MainView.xaml.cs
@@ -11,17 +11,66 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Wpf.Ui.Appearance;
+using Wpf.Ui.Controls;
+using Wpf.Ui.Controls.Interfaces;
+using Wpf.Ui.Mvvm.Contracts;
 
 namespace ClashF.View
 {
     /// <summary>
     /// MainView.xaml 的交互逻辑
     /// </summary>
-    public partial class MainView : Window
+    public partial class MainView : UiWindow, INavigationWindow
     {
-        public MainView()
+        private readonly IThemeService _themeService;
+
+        public MainView(IThemeService themeService)
         {
+            // Attach the theme service
+            _themeService = themeService;
+
             InitializeComponent();
         }
+
+        private void RootNavigation_OnNavigated(Wpf.Ui.Controls.Interfaces.INavigation sender, Wpf.Ui.Common.RoutedNavigationEventArgs e)
+        {
+
+        }
+
+        private void NavigationButtonTheme_OnClick(object sender, RoutedEventArgs e)
+        {
+            _themeService.SetTheme(_themeService.GetTheme() == ThemeType.Dark ? ThemeType.Light : ThemeType.Dark);
+        }
+
+        private void TrayMenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (sender is not System.Windows.Controls.MenuItem menuItem)
+                return;
+
+            System.Diagnostics.Debug.WriteLine($"DEBUG | WPF UI Tray clicked: {menuItem.Tag}", "Wpf.Ui.Demo");
+        }
+
+        #region INavigationWindow methods
+
+        public Frame GetFrame()
+            => RootFrame;
+
+        public INavigation GetNavigation()
+            => RootNavigation;
+
+        public bool Navigate(Type pageType)
+            => RootNavigation.Navigate(pageType);
+
+        public void SetPageService(IPageService pageService)
+            => RootNavigation.PageService = pageService;
+
+        public void ShowWindow()
+            => Show();
+
+        public void CloseWindow()
+            => Close();
+
+        #endregion INavigationWindow methods
     }
 }
